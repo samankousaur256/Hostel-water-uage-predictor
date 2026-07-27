871ae28b26753a7b4f2544eeebee324f9c50473a
samankousaur <samankousaur2006@gmail.com>
2026-07-27 23:32:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="Hostel Water Usage Predictor")
 
 st.title(" hostel water usage predictor")
 
-students = st.number_input("number of students",main_value=1, value=100)
+students = st.number_input("number of students",min_value=1, value=100)
 water_per_student = st.number_input("water usage per student (liters)", min_value=1, value=135)
 
 if st.button("predict"):
