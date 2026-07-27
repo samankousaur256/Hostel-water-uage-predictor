5635d8524c949617aade4f8c49a3fe76fb6c4ee7
samankousaur <samankousaur2006@gmail.com>
2026-07-28 00:24:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 
-st.set_page_config(page_title="Hostel Water Usage Predictor")
-
-st.title(" hostel water usage predictor")
+st.title("hostel water usage predictor")
 
 students = st.number_input("number of students",min_value=1, value=100)
 water_per_student = st.number_input("water usage per student (liters)", min_value=1, value=135)
-
+                                    
 if st.button("predict"):
-    total = students * water_per_student
-    st.success(f"Estimated Daily Water Usage: {total} liters")
+   total = students * water_Per_student
+
+   st.success(f"Estimated daily water usage:{total} liters")
+
     fig, ax = plt.subplots()
-     ax.bar(["water usage"], [total])
-     ax.set_ylabel("liters")
-     ax.set_title("hostel daily water usage")
-     st.pyplot(fig)
+    ax.bar(["water usage"], [total])
+    ax.set_ylabel("liters")
+    ax.set_title("hostel daily water usage")
+
+    st.pyplot(Fig)
