9f55eaaea815eb21b614ec0c1bee9f8f8a32a923
samankousaur <samankousaur2006@gmail.com>
2026-07-28 00:31:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if st.button("predict"):
 
    st.success(f"Estimated daily water usage:{total} liters")
  
-    fig, ax = plt.subplots()
+  fig, ax = plt.subplots()
    
     ax.bar(["water usage"], [total])
     ax.set_ylabel("liters")
