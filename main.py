959fad4c78a1029a4717bfb943cfc30768c8b545
samankousaur <samankousaur2006@gmail.com>
2026-07-27 23:59:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ students = st.number_input("number of students",min_value=1, value=100)
 water_per_student = st.number_input("water usage per student (liters)", min_value=1, value=135)
 
 if st.button("predict"):
-  total = students * water_per_student
-  st.success(f"Estimated Daily Water Usage: {total} liters")
+    total = students * water_per_student
+    st.success(f"Estimated Daily Water Usage: {total} liters")
  
- fig, ax = plt.subplots()
- ax.bar(["water usage"], [total])
- ax.set_ylabel("liters")
- ax.set_title("hostel daily water usage")
-st.pyplot(fig)
+   fig, ax = plt.subplots()
+
+   ax.bar(["water usage"], [total])
+
+   ax.set_ylabel("liters")
+
+   ax.set_title("hostel daily water usage")
+
+   st.pyplot(fig)
