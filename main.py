c6c7e3145e86a4003f9ae9dd7203139d78b9a63c
samankousaur <samankousaur2006@gmail.com>
2026-07-27 23:18:23 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+st.set_page_config(page_title="Hostel Water Usage Predictor")
+
+st.title(" hostel water usage predictor")
+
+students = st.number_input("number of students",main_value=1, value=100)
+water_per_student = st.number_input("water usage per student (liters)", min_value=1, value=135)
+
+if st.button("predict"):
+  total = students * water_per_student
+  st.success(f"estimated daily water usage:
+{total} liter")
