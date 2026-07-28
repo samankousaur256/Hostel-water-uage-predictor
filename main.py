2773ff9558b757b3403e938682eb63b52551f0c8
samankousaur <samankousaur2006@gmail.com>
2026-07-28 23:14:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ st.write("""
 ✅ Easy to understand and use
 """)
 students = st.number_input("number of students",min_value=1, value=100)
-water_per_student = st.number_input("water usage per student (liters)", min_value=1, value=135)
+water_per_student = st.number_input("water usage per student (liters/day)", min_value=1, value=135)
                                     
 if st.button("predict"):
   
