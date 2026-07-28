a493758ed6e8aae756ceb5dc77cd07e2122bfb54
samankousaur <samankousaur2006@gmail.com>
2026-07-28 21:55:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,13 @@ if st.button("predict"):
    total = students * water_Per_student
 
    st.success(f"Estimated daily water usage:{total} liters")
-   st.subheader("water usage graph")
+   predicted_value = float(prediction)
 
    fig, ax = plt.subplots(figsize=(6, 4))
 
-   labels = ["predicted water usage"]
-   values = [prediction[0]]
-
-   ax.bar(labels, values, colours="skyblue")
-
-   ax.set_xlabel("prediction")
-   ax.set_ylabel("water usage (liters)")
-   ax.set_title("predicted hostel water usage")
+   ax.bar(["water usage"], [predicted_value], colour="green")
+  
+   ax.set_ylabel("liters")
+   ax.set_title("predicted water usage")
 
    st.pyplot(fig)
