ed876279f407d04f68833a41ba97876dfb888913
samankousaur <samankousaur2006@gmail.com>
2026-07-28 22:46:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ students = st.number_input("number of students",min_value=1, value=100)
 water_per_student = st.number_input("water usage per student (liters)", min_value=1, value=135)
                                     
 if st.button("predict"):
+  
    total = students * water_Per_student
 
    st.success(f"Estimated daily water usage:{total} liters")
-   predicted_value = float(prediction)
+  
+   predicted_value = total
 
    fig, ax = plt.subplots(figsize=(6, 4))
 
